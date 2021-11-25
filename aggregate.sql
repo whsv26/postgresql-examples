515be9f3378aa24e836a7e21d52b8066fad3acd4
whsv26 <whsv26@gmail.com>
2021-11-26 00:57:13 +0300

    [Example] Custom aggregate function.

---
+++ aggregate.sql
@@ -0,0 +1,16 @@
+create aggregate jsonb_merge(jsonb) (
+  SFUNC = jsonb_concat,
+  STYPE = jsonb,
+  INITCOND = '{}'
+);
+
+with input(id, obj) as (values
+    (1, '{"x": 1}'::jsonb),
+    (1, '{"x": 2, "y": 1}'::jsonb),
+    (1, '{"x": 3, "y": 2}'::jsonb)
+)
+select
+    id,
+    jsonb_merge(obj)
+from input
+group by id;
