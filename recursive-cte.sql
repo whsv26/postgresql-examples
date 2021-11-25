f01ea4da688f65d4cf1455a98f2dcea14a16b474
whsv26 <whsv26@gmail.com>
2021-11-25 22:39:04 +0300

    [Example] Recursive CTE.

--- recursive-cte.sql
+++ recursive-cte.sql
@@ -1,16 +1,23 @@
 with recursive
 
 input(id, parent_id, level) as (values
-    (1, null, 'level 1'),
+    (1, 0, 'level 1'),
     (2, 1, 'level 2'),
     (3, 2, 'level 3'),
-    (4, 3, 'level 4'),
+    (4, 3, 'level 4')
 ),
 
--- hierarchy as (
---     select
---     from input i
--- )
---
--- select *
--- from input
+hierarchy as (
+    select id, parent_id, level
+    from input
+    where id = 3 -- start point
+    union all
+    -- stop when join result is empty set (0 rows returned)
+    select i.id, i.parent_id, i.level
+    from hierarchy h -- previous result set stored in temporary table
+        join input i
+            on i.id = h.parent_id
+)
+
+select *
+from hierarchy
